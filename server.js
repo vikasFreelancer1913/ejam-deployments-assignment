0943dff9fd21d75883291329c11bc0ac7b49ad6e
Vikas Singh <vikas@fieldcastmedia.com>
2020-06-06 03:25:03 +0530

    error handling

--- server.js
+++ server.js
@@ -8,16 +8,14 @@ const PORT = process.env.PORT || 3001;
 const HOST = process.env.LOCAL_HOST || '0.0.0.0';
 const MONGODB_URI = process.env.PROD_MONGODB;
 // app.use(cors())
+app.use('/api', router);
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
-
 if(process.env.NODE_ENV === 'production') {  
   app.use(express.static(path.join(__dirname, 'client/build')));
 }
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', router);
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
 mongoose.connection.once('open', function() {
   console.log('Connected to the Database.');
