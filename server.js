c297a07e9f9ca3f135465dc8fcf632388ad7448f
Vikas Singh <vikas@fieldcastmedia.com>
2020-06-06 01:51:07 +0530

    Server.js full change

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require('express'); 
+const express = require('express');
 const mongoose = require('mongoose');
 // const cors = require('cors');
 const router = require('./routes/index');
 
-const app = express(); 
+const app = express();
 const PORT = process.env.PORT || 3001;
+const HOST = process.env.LOCAL_HOST || '0.0.0.0';
 const MONGODB_URI = process.env.PROD_MONGODB;
-
 // app.use(cors())
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', router); 
+app.use('/api', router);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
-mongoose.connection.once('open', function() { 
+mongoose.connection.once('open', function() {
   console.log('Connected to the Database.');
 });
+
 mongoose.connection.on('error', function(error) {
   console.log('Mongoose Connection Error : ' + error);
 });
 
-app.listen(PORT, function() { 
+app.listen(PORT, HOST, function() {
   console.log(`Server listening on port ${PORT}.`);
 });
