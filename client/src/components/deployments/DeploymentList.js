6495e35ee31a7e894cc261fac515f71d53e14183
Vikas Singh <vikas@fieldcastmedia.com>
2020-06-06 10:12:34 +0530

    CSS fixed

--- client/src/components/deployments/DeploymentList.js
+++ client/src/components/deployments/DeploymentList.js
@@ -26,42 +26,62 @@ function DeploymentList(props) {
       <hr/>
       {
         deployments && deployments.length ?
-        <table className="deploymentTable">
-          <thead>
-            <tr>
-              <th>
-                Template Name
-              </th>
-              <th>
-                URL
-              </th>
-              <th>
-                Version
-              </th>
-              <th>
-                Deployed At
-              </th>
-              <th>
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {deployments && deployments.length > 0 && deployments.map(function(deployment) { 
-              return (
-                <tr key={ deployment._id }> 
-                  <td>{deployment.name}</td> 
-                  <td>{deployment.url}</td>
-                  <td>{deployment.version}</td>
-                  <td>{deployment.deployedAt}</td>
-                  <td className="actions">
-                    <button onClick={() => handleDelete(deployment._id)}><FaTrashAlt /></button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table> 
+        <div>
+          <table className="deploymentTable">
+            <thead>
+              <tr>
+                <th>
+                  Template Name
+                </th>
+                <th>
+                  URL
+                </th>
+                <th>
+                  Version
+                </th>
+                <th>
+                  Deployed At
+                </th>
+                <th>
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {deployments && deployments.length > 0 && deployments.map(function(deployment) { 
+                return (
+                  <tr key={ deployment._id }> 
+                    <td>{deployment.name}</td> 
+                    <td>{deployment.url}</td>
+                    <td>{deployment.version}</td>
+                    <td>{deployment.deployedAt}</td>
+                    <td className="actions">
+                      <button onClick={() => handleDelete(deployment._id)}><FaTrashAlt /></button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table> 
+
+          <div className="dataForMobile">
+              <ul>
+                {deployments && deployments.length > 0 && deployments.map(function(deployment) { 
+                  return (
+                    <li key={ deployment._id }> 
+                      <p><span>Template name </span>{deployment.name}</p> 
+                      <p><span>URL </span>{deployment.url}</p>
+                      <p><span>Version </span>{deployment.version}</p>
+                      <p><span>Deployed At </span>{deployment.deployedAt}</p>
+                      <p className="actions">
+                        <button onClick={() => handleDelete(deployment._id)}><FaTrashAlt /></button>
+                      </p>
+                    </li>
+                  );
+                })}
+              </ul>
+          </div>
+        </div>
         :
         <p className="noDataMessage">No deployment at present. Proceed and <Link to="/deployments/new">Create Deployment</Link></p>
       }
